f1549deae69132b41733834cf01b2d1e0729b036
Giada <tonni.giada.210802@gmail.com>
2026-02-16 14:57:26 +0100

    SECURITYCONFIG FATTO

---
+++ src/main/java/giadatonni/CONSEGNA_S19L1/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package giadatonni.CONSEGNA_S19L1.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
+        httpSecurity.formLogin(form -> form.disable());
+        httpSecurity.csrf(csrf -> csrf.disable());
+        httpSecurity.sessionManagement(sessions -> sessions.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        httpSecurity.authorizeHttpRequests(req -> req.requestMatchers("/**").permitAll());
+
+        return httpSecurity.build();
+    }
+}
